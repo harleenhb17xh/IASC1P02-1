ef26c239a77df2955f6921403a372ecfb8736031
Harleen <hb17xh@brocku.ca>
2019-12-03 01:59:51 -0500

    final assignment

---
+++ scriptfinal.js
@@ -0,0 +1,13 @@
+function press(){
+onclick="document.getElementById('press').innerHTML = Date()}
+
+function box(){
+
+}
+<script>
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
